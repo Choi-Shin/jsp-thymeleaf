00f62963c336ce5115ec32002f29744bb6c7571f
Choi-Shin <shincho1990@naver.com>
2024-06-13 20:33:57 +0900

    first

---
+++ src/main/java/com/example/jsp_thymeleaf/JspThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.jsp_thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JspThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JspThymeleafApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jsp_thymeleaf/JspThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jsp_thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JspThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
